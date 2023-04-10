b5bfdd8962b9778d15d1f901f3961876e99279c6
rubeenapk <rubeenaanwar5354@gmail.com>
2023-04-10 16:30:39 +0530

    parent details updated

--- src/components/parentDetails/ParentDetails.js
+++ src/components/parentDetails/ParentDetails.js
@@ -4,68 +4,10 @@ import profile from "../../images/profile.jpg";
 import { FaHome } from "react-icons/fa";
 
 const ParentDetails = () => {
-  // const [data, setData] = useState([
-  //   {
-  //     name: "Sathyan PT",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  //   {
-  //     name: "Prajesh KT",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  //   {
-  //     name: "Zinumol Jamal",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  //   {
-  //     name: "Mike Brown",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  //   {
-  //     name: "John Doe",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  //   {
-  //     name: "Jane Smith",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  //   {
-  //     name: "John Doe",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  //   {
-  //     name: "Jane Smith",
-  //     number: 564654564,
-  //     email: "demoparent@zaeemsolutions.com",
-  //     created: "14/01/2023",
-  //     action: "",
-  //   },
-  // ]);
   return (
-    <div className="header">
+    <div className="main-div">
       <div className="sub-header">
-        <div>
+        <div className="parent-head">
           <h4>Parent</h4>
         </div>
         <div className="parent-list">
@@ -76,11 +18,10 @@ const ParentDetails = () => {
           <h6>* Parent Details</h6>
         </div>
       </div>
-
       <div className="content-box">
-        <div classname="parent">
-          
-          <div className="child-div">
+        <div classname="parent-table">
+          <div className="parent">
+            <div classname="parent2">
             <div className="parent-details">
               <div className="parent-pic">
                 <img src={profile} alt="Profile" />
@@ -94,75 +35,75 @@ const ParentDetails = () => {
                   <h6>Email:</h6>
                   <h6>DOB:</h6>
                 </div>
+            </div>
+            </div>
+            <div className="address">
+              <div className="permanent-address">
+                <h3>
+                  <b>Permanent Address</b>
+                </h3>
+                <h6>bhedhsaidha bhavan asd</h6>
+                <h6>asdasd india</h6>
+                <h6>56788</h6>
+              </div>
+              <div className="postal-address">
+                <h3>
+                  <b>Postal Address</b>
+                </h3>
               </div>
             </div>
-            <hr />
-            {/* <div className="column"> */}
-              <div className="address">
-                <div className="permanent-address">
-                  <h3>
-                    <b>Permanent Address</b>
-                  </h3>
-                  <h6>bhedhsaidha bhavan asd</h6>
-                  <h6>asdasd india</h6>
-                  <h6>56788</h6>
-                </div>
-                <div className="postal-address">
-                  <h3>
-                    <b>Postal Address</b>
-                  </h3>
-                </div>
-              </div>  
-              <div className="authentication">
-                <div className="authentication-details">
-                  <h6>
-                    <b>Authentication Details</b>
-                  </h6>
-                </div>
-                <input type="text" placeholder="username:" />
-                <div className="authentication-details">
-                  <h6>
-                    <b>Notifications</b>
-                  </h6>
-                  <h6>SMS</h6>
-                  <h6>
-                    Bus: <span id="disabledText">Disabled</span>
-                  </h6>
-                  <h6>
-                    News:<span id="disabledText">Disabled</span>
-                  </h6>
-                  <h6>
-                    Notice:<span id="enabledText">Enabled</span>
-                  </h6>
-                </div>
+            </div>
+            <div className="authentication">
+              <div className="auth-items">
+              <h6>
+                <b>Authentication Details</b>
+              </h6>
+              <input type="text" id="inputbox" placeholder="username:" />
+              <h6>
+                <br/>
+                <b>Notifications</b>
+              </h6>
+              <h6>SMS</h6>
+              <br/>
+              <h6>
+                Bus: <span id="disabledText">Disabled</span>
+              </h6>
+              <h6>
+                News:<span id="disabledText">Disabled</span>
+              </h6>
+              <h6>
+                Notice:<span id="enabledText">Enabled</span>
+              </h6>
               </div>
-            {/* </div> */}
-          </div>
-        </div>
+            </div>
+          </div>  
 
-        <table className="table">
-          <tr>
-            <th>Students No</th>
-            <th>Admission Class</th>
-            <th>Vehicle Id</th>
-            <th>Student Name</th>
-            <th>Mobile</th>
-            <th>Email</th>
-            <th>Admission Date</th>
-          </tr>
-          <tr>
-            <td>24</td>
-            <td>3rd</td>
-            <td>1</td>
-            <td>vysh3</td>
-            <td>+917012962320</td>
-            <td>sinu.jamal@zaeemsolutions.com</td>
-            <td>12-02-2022</td>
-          </tr>
-        </table>
+          <table className="table">
+            <tr>
+              <th>Students No</th>
+              <th>Admission Class</th>
+              <th>Vehicle Id</th>
+              <th>Student Name</th>
+              <th>Mobile</th>
+              <th>Email</th>
+              <th>Admission Date</th>
+            </tr>
+            <tr>
+              <td>24</td>
+              <td>3rd</td>
+              <td>1</td>
+              <td>vysh3</td>
+              <td>+917012962320</td>
+              <td>sinu.jamal@zaeemsolutions.com</td>
+              <td>12-02-2022</td>
+            </tr>
+          </table>
+        </div>
       </div>
-      <button>Cancel</button>
-      <button>Edit</button>
+      <div className="buttons">
+        <button className="cancel">Cancel</button>
+        <button className="edit">Edit</button>
+      </div>
     </div>
   );
 };
---
+++ src/components/parentDetails/dcfgvhj.js
@@ -0,0 +1,105 @@
+<div className="header">
+      {/* <div className="sub-header">
+        <div>
+          <h4>Parent</h4>
+        </div>
+        <div className="parent-list">
+          <h6>
+            <FaHome className="fa-house" />
+            Dashboard
+          </h6>
+          <h6>* Parent Details</h6>
+        </div>
+      </div> */}
+
+      <div className="content-box">
+        <div classname="parent">
+          <div className="parent-main">
+            <div className="parent-details">
+              <div className="parent-pic">
+                <img src={profile} alt="Profile" />
+              </div>
+              <div className="details">
+                <div className="parent-name">
+                  <h3>Geetha</h3>
+                </div>
+                <div className="other-details">
+                  <h6>Phone:</h6>
+                  <h6>Email:</h6>
+                  <h6>DOB:</h6>
+                </div>
+              </div>
+            </div>
+            <hr />
+            {/* <div className="column"> */}
+              <div className="address">
+                <div className="permanent-address">
+                  <h3>
+                    <b>Permanent Address</b>
+                  </h3>
+                  <h6>bhedhsaidha bhavan asd</h6>
+                  <h6>asdasd india</h6>
+                  <h6>56788</h6>
+                </div>
+                <div className="postal-address">
+                  <h3>
+                    <b>Postal Address</b>
+                  </h3>
+                </div>
+              </div>  
+              </div>
+              <div className="authentication">
+                <div className="authentication-details">
+                  <h6>
+                    <b>Authentication Details</b>
+                  </h6>
+                </div>
+                <input type="text" placeholder="username:" />
+                <div className="authentication-details2">
+                  <h6>
+                    <b>Notifications</b>
+                  </h6>
+                  <h6>SMS</h6>
+                  <h6>
+                    Bus: <span id="disabledText">Disabled</span>
+                  </h6>
+                  <h6>
+                    News:<span id="disabledText">Disabled</span>
+                  </h6>
+                  <h6>
+                    Notice:<span id="enabledText">Enabled</span>
+                  </h6>
+                </div>
+              </div>
+            {/* </div> */}
+          
+        </div>
+
+        <table className="table">
+          <tr>
+            <th>Students No</th>
+            <th>Admission Class</th>
+            <th>Vehicle Id</th>
+            <th>Student Name</th>
+            <th>Mobile</th>
+            <th>Email</th>
+            <th>Admission Date</th>
+          </tr>
+          <tr>
+            <td>24</td>
+            <td>3rd</td>
+            <td>1</td>
+            <td>vysh3</td>
+            <td>+917012962320</td>
+            <td>sinu.jamal@zaeemsolutions.com</td>
+            <td>12-02-2022</td>
+          </tr>
+        </table>
+      </div>
+      <div className="buttons">
+      <button className="cancel">Cancel</button>
+      <button className="edit">Edit</button>
+      </div>
+    </div>
+   
+    
