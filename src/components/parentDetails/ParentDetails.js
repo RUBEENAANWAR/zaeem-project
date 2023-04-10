520cc47d95cf23771a28d363d5eb5ca97ea7f22f
rubeenapk <rubeenaanwar5354@gmail.com>
2023-04-10 14:11:12 +0530

    updated parent details

--- src/components/parentDetails/ParentDetails.js
+++ src/components/parentDetails/ParentDetails.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ParentDetails.css";
 import profile from "../../images/profile.jpg";
-import {FaHome } from 'react-icons/fa';
+import { FaHome } from "react-icons/fa";
 
 const ParentDetails = () => {
   // const [data, setData] = useState([
@@ -65,61 +65,81 @@ const ParentDetails = () => {
   return (
     <div className="header">
       <div className="sub-header">
-        <div><h4>Parent</h4></div>
-       <div className="parent-list">
-       <h6><FaHome className="fa-house" />Dashboard</h6>
-       <h6>* Parent Details</h6>
-        </div> 
-        
+        <div>
+          <h4>Parent</h4>
+        </div>
+        <div className="parent-list">
+          <h6>
+            <FaHome className="fa-house" />
+            Dashboard
+          </h6>
+          <h6>* Parent Details</h6>
+        </div>
       </div>
- 
+
       <div className="content-box">
-        <div className="parent-details">
-          <div className="parent-pic">
-            <img src={profile} alt="Profile" />
-          </div>
-          <div className="details">
-            <div className="parent-name">
-              <h3>Geetha</h3>
+        <div classname="parent">
+          
+          <div className="child-div">
+            <div className="parent-details">
+              <div className="parent-pic">
+                <img src={profile} alt="Profile" />
+              </div>
+              <div className="details">
+                <div className="parent-name">
+                  <h3>Geetha</h3>
+                </div>
+                <div className="other-details">
+                  <h6>Phone:</h6>
+                  <h6>Email:</h6>
+                  <h6>DOB:</h6>
+                </div>
+              </div>
             </div>
-            <div className="other-details">
-              <h6>Phone:</h6>
-              <h6>Email:</h6>
-              <h6>DOB:</h6>
-            </div>
+            <hr />
+            {/* <div className="column"> */}
+              <div className="address">
+                <div className="permanent-address">
+                  <h3>
+                    <b>Permanent Address</b>
+                  </h3>
+                  <h6>bhedhsaidha bhavan asd</h6>
+                  <h6>asdasd india</h6>
+                  <h6>56788</h6>
+                </div>
+                <div className="postal-address">
+                  <h3>
+                    <b>Postal Address</b>
+                  </h3>
+                </div>
+              </div>  
+              <div className="authentication">
+                <div className="authentication-details">
+                  <h6>
+                    <b>Authentication Details</b>
+                  </h6>
+                </div>
+                <input type="text" placeholder="username:" />
+                <div className="authentication-details">
+                  <h6>
+                    <b>Notifications</b>
+                  </h6>
+                  <h6>SMS</h6>
+                  <h6>
+                    Bus: <span id="disabledText">Disabled</span>
+                  </h6>
+                  <h6>
+                    News:<span id="disabledText">Disabled</span>
+                  </h6>
+                  <h6>
+                    Notice:<span id="enabledText">Enabled</span>
+                  </h6>
+                </div>
+              </div>
+            {/* </div> */}
           </div>
         </div>
-        <hr />
-        <div className="column">
-        <div className="address">
-          <div className="permanent-addrress">
-            <h3>
-              <b>Permanent Address</b>
-            </h3>
-            <h6>bhedhsaidha bhavan asd</h6>
-            <h6>asdasd india</h6>
-            <h6>56788</h6>
-          </div>
-          <div className="postal-address">
-            <h3>
-              <b>Postal Address</b>
-            </h3>
-          </div>
-          <div className="authentication">
-            <div className="authentication-details">
-              <h6><b>Authentication Details</b></h6>
-            </div>
-            <input type="text" placeholder="username:" />
-            <div className="authentication-details">
-              <h6><b>Notifications</b></h6>
-              <h6>SMS</h6>
-              <h6>Bus: <span id="disabledText">Disabled</span></h6>
-              <h6>News:<span id="disabledText">Disabled</span></h6>
-              <h6>Notice:<span id="enabledText">Enabled</span></h6>
-            </div>
-          </div>
-        </div>
-        </div>
+
         <table className="table">
           <tr>
             <th>Students No</th>
